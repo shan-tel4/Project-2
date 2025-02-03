1d8cd4649dc0b017155550acbeac29a2c5bff6cb
Shantel Shaw <shantel_shaw@hotmail.co.uk>
2025-02-03 09:37:39 +0000

    register admin models

--- myproject/myapp/admin.py
+++ myproject/myapp/admin.py
@@ -1,3 +1,11 @@
-from django.contrib import admin
 
-# Register your models here.
+from django.contrib import admin
+from .models import User, AudioFile, Session, Deck, DeckAssignment  # Import all models
+
+# Register each model
+admin.site.register(User)
+admin.site.register(AudioFile)
+admin.site.register(Session)
+admin.site.register(Deck)
+admin.site.register(DeckAssignment)
+
