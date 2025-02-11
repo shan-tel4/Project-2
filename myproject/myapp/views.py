a2759343d92b9839805418d32906afc6eb9f22e6
Shantel Shaw <shantel_shaw@hotmail.co.uk>
2025-02-11 09:34:21 +0000

    frontend intergrated with backend

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -1,16 +1,13 @@
-
-# Create your views here.
 from django.shortcuts import render
 
 def login_view(request):
     return render(request, 'login.html')
 
-def session_view(request):
-    return render(request, 'session.html')
+def signup_view(request):
+    return render(request, 'signup.html')
 
 def contact_view(request):
     return render(request, 'contact.html')
 
-def signup_view(request):
-    return render(request, 'signup.html')
-
+def session_view(request):
+    return render(request, 'session.html')
--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -3,5 +3,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('myapp.urls')),  # Include your app's URLs here
+    path('', include('myapp.urls')),  # Adjust the app name if necessary
 ]
