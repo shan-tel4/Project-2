705134e13de5014ef76dc2d9375ce649c2250139
Shantel Shaw <shantel_shaw@hotmail.co.uk>
2025-02-12 20:59:35 +0000

    login authentication added

--- myproject/myapp/urls.py
+++ myproject/myapp/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.homepage_view, name='homepage'),  # Root URL
-    path('login/', views.login_view, name='login'),
-    path('signup/', views.signup_view, name='signup'),
+    path('', views.signup_view, name='signup'),
+    path('signup', views.signup_view, name='login'),  # Root URL
+    path('login/', views.homepage_view, name='homepage'),
     path('contact/', views.contact_view, name='contact'),
     path('session/', views.session_view, name='session'),
 ]
--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -1,9 +1,41 @@
 from django.shortcuts import render
+from django.contrib import messages
+from django.shortcuts import redirect
 
 def login_view(request):
     return render(request, 'login.html')
 
 def signup_view(request):
+    from django.contrib.auth.models import User  # Import inside the function
+
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        confirm_password = request.POST['confirm-password']
+        if password == confirm_password:
+            try:
+                user = User.objects.create_user(username=username, email=email, password=password)
+                print("trying to save")
+                user.save()
+                # login(request, user)
+                print("saved user")
+
+                messages.success(request, 'signup successfully')
+                return redirect('contact')
+            except Exception as e:
+                print("Error", e)
+                messages.error(request, 'could not signup')
+                # return render(request, 'signup.html', {'error': 'An error occurred'})
+        else:
+            print("error ")
+            messages.error(request, 'could not piiiiiii')
+            return render(request, 'signup.html', {'error': 'Password should match'})
+    else:
+         return render(request, 'signup.html', {'error': 'An error occurred'}) 
+
+    print("nowww")
+   
     return render(request, 'signup.html')
 
 def contact_view(request):
