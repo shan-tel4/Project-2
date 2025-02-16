5fbf89420379d1d5c4d694960937d8ff3fe21fdf
Shantel Shaw <shantel_shaw@hotmail.co.uk>
2025-02-16 14:11:19 +0000

    log in page created

--- api.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-// Define your API routes here
-router.get('/', (req, res) => {
-  res.send('API is working!');
-});
-
-module.exports = router; // Ensure the router is exported
