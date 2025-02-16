795091c5f8ecff4e93864c6d1eacba9c33f34a03
Shantel Shaw <shantel_shaw@hotmail.co.uk>
2025-02-16 14:17:19 +0000

    homepage html removed

--- myproject/myapp/urls.py
+++ myproject/myapp/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.signup_view, name='signup'),
-    path('signup', views.signup_view, name='login'),  # Root URL
-    path('login/', views.homepage_view, name='homepage'),
+    path('login/', views.login_view, name='login'),
+    path('signup', views.signup_view, name='signup'),
     path('contact/', views.contact_view, name='contact'),
     path('session/', views.session_view, name='session'),
 ]
